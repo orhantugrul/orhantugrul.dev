0e8ed7a7fd345d93e063f0a67dd82df211f5f29e
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2023-03-08 23:39:09 +0300

    Add meta tags for seo optimization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
-<script>
+<script lang="ts">
   import "../app.css";
   import Navbar from "$lib/components/Navbar";
+  import Footer from "$lib/components/Footer";
 </script>
+
+<svelte:head>
+  <title>Orhan Tugrul &bull; A Software Developer</title>
+  <meta
+    name="description"
+    content="Highly motivated software developer with experience in building high-quality applications using Java, Kotlin, and JavaScript."
+  />
+</svelte:head>
 
 <Navbar />
 <slot />
+<Footer />
 
 <style lang="postcss">
   :global(body) {

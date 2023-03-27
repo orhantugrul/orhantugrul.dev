860a09c0bf1fad98806a337b5ac54aee2659be3b
orhantugrul <orhan.tugrul.61@gmail.com>
2023-03-27 22:07:39 +0300

    Fix stying issue to apply common component

---
+++ src/routes/tech-stack/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Container from "$lib/layouts/Container";
+</script>
+
+<Container
+  title={"Contact • Orhan Tugrul"}
+  description={"My current personal tech stack: Tools & Platforms"}
+/>

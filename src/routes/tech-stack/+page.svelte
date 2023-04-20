9c59166e9640fd9100433f96ece59c3f0bd0fbd0
orhantugrul <orhan.tugrul.61@gmail.com>
2023-04-20 13:31:25 +0300

    Add new about page instead of contact and tech-stack

--- src/routes/tech-stack/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import Container from "$lib/layouts/Container";
-</script>
-
-<Container
-  title={"Tech Stack • Orhan Tugrul"}
-  description={"My current personal tech stack: Tools & Platforms"}
-/>

459ca43f9a06e38bab87c5a7bfacf289eebcddf0
orhantugrul <orhan.tugrul.61@gmail.com>
2023-10-22 21:58:41 +0300

    remove: Button, Icon, Main, Navbar components

--- src/lib/components/Icon/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ArrowIcon } from "./ArrowIcon.svelte";
-export { default as GithubIcon } from "./GithubIcon.svelte";
-export { default as HackerRankIcon } from "./HackerRankIcon.svelte";
-export { default as OnlyFansIcon } from "./OnlyFansIcon.svelte";
--- src/lib/components/Main/Main.svelte
+++
@@ -1,3 +0,0 @@
-<main class="flex flex-col flex-auto min-w-0 mt-6 md:mt-0 px-2 md:px-0">
-  <slot />
-</main>

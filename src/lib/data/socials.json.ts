50935e6aa3d4f8699cab44a1d92dd2311985de5c
orhantugrul <orhan.tugrul.61@gmail.com>
2023-10-22 22:00:36 +0300

    feat: add experiences and socials dat

---
+++ src/lib/data/socials.json.ts
@@ -0,0 +1,21 @@
+export type Social = {
+  title: string;
+  link: string;
+};
+
+const socials: Social[] = [
+  {
+    title: "Email",
+    link: "orhan.tugrul.61@gmail.com",
+  },
+  {
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/orhantugrul",
+  },
+  {
+    title: "GitHub",
+    link: "https://github.com/orhantugrul",
+  },
+];
+
+export default socials;

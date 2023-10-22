266e6b08a8c7b4dda55957358fbe6509d59aefb5
orhantugrul <orhan.tugrul.61@gmail.com>
2023-10-22 21:59:52 +0300

    refactor: Link component to remove unnecessary code and improve styling

--- src/lib/components/Link/Link.svelte
+++ src/lib/components/Link/Link.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-  import ArrowIcon from "../Icon/ArrowIcon.svelte";
-
-  export let title: string;
   export let href: string;
 </script>
 
 <a
-  class="flex items-center hover:text-neutral-200 transition-all"
+  class="underline hover:no-underline duration-200 after:content-['_↗']"
   target="_blank"
   rel="noopener noreferrer"
   {href}
 >
-  <ArrowIcon class="mr-2 mb-1" />
-  <p class="h-7">{title}</p>
+  <slot />
 </a>

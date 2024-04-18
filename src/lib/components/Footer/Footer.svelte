60cc46b58894494481e7a85ef8cfea9546e5f0a2
orhantugrul <orhantugrulsahin@gmail.com>
2024-04-18 23:25:30 +0300

    feat: add footer component and export it from index file

---
+++ src/lib/components/Footer/Footer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let trabzonTime = getTrabzonTime();
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      trabzonTime = getTrabzonTime();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  function getTrabzonTime() {
+    const options = {
+      timeZone: "Europe/Istanbul",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    } as Intl.DateTimeFormatOptions;
+
+    return new Date().toLocaleString("en-US", options);
+  }
+</script>
+
+<footer class="flex border-t-[1px] border-[#2E3036]">
+  <div class="flex flex-col py-4">
+    <p class="dark:text-[#CFD1D6] text-xs font-mono mb-3">
+      Trabzon, {trabzonTime}
+    </p>
+
+    <p class="dark:text-[#CFD1D6] text-xs font-mono">
+      © {new Date().getFullYear()} Orhan Tuğrul Şahin
+    </p>
+  </div>
+</footer>

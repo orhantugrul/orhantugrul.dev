5de089a6c92ab6880ffba572189a1af65d74f0ac
orhantugrul <orhantugrulsahin@gmail.com>
2024-05-11 16:46:28 +0300

    chore: remove unused data file and clean up config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
-import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter(),
-    alias: {
-      $data: path.resolve("./src/data"),
-    },
   },
 };
 

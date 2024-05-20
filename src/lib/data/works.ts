fc5d32f7f6e4032a5c93e08a9c12a4fb56209641
orhantugrul <orhantugrulsahin@gmail.com>
2024-05-20 22:39:45 +0300

    feat: add works data structure and initial data

---
+++ src/lib/data/works.ts
@@ -0,0 +1,20 @@
+export type Work = {
+  title: string;
+  description: string;
+  link: string;
+};
+
+const works: Work[] = [
+  {
+    title: "Cloud4Next",
+    description: "Multi-cloud management solution for businesses",
+    link: "https://www.cloud4next.com/",
+  },
+  {
+    title: "Spinex",
+    description: "Treasury solution that automates financial processes",
+    link: "https://www.linktera.com/spinex-labs",
+  },
+];
+
+export default works;

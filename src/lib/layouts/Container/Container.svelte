6b55a69b3e5e41ebda35d80ab308e467d8557c69
orhantugrul <orhantugrulsahin@gmail.com>
2024-06-10 23:49:39 +0300

    feat: add Header and Footer components to Container layout

--- src/lib/layouts/Container/Container.svelte
+++ src/lib/layouts/Container/Container.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Footer from "$lib/components/Footer/Footer.svelte";
+  import Header from "$lib/components/Header/Header.svelte";
   import "../../../app.css";
 
   export let title: string;
@@ -10,6 +12,10 @@
   <meta name="description" content={description} />
 </svelte:head>
 
-<main class="flex flex-col flex-auto">
-  <slot />
+<main class="flex flex-col px-4">
+  <div class="flex flex-col flex-auto mx-auto max-w-[540px]">
+    <Header />
+    <slot />
+    <Footer />
+  </div>
 </main>

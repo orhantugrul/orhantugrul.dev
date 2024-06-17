fded1ecf3395946075ab3cb8bb35ca2be9335772
orhantugrul <orhantugrulsahin@gmail.com>
2024-06-17 23:30:20 +0300

    feat: add Header component with navigation links

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const pages = [
+    {
+      title: "Home",
+      link: "/",
+    },
+    {
+      title: "Blog",
+      link: "/blog",
+    },
+  ];
+</script>
+
+<header
+  class="my-5 flex items-center justify-between border-b-[1px] border-outline"
+>
+  <nav class="flex items-center py-4">
+    {#each pages as { title, link }}
+      <a href={link}>
+        <span
+          class="border-b-[1px] border-bright font-mono text-xs dark:text-bright"
+        >
+          {title}
+        </span>
+      </a>
+    {/each}
+  </nav>
+
+  <div>
+    <button class="font-mono text-xs text-gray">Lights on</button>
+  </div>
+</header>
